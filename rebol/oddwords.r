a9874c4a3b78752480bba6b081b93b6239b2b7b8
Marcelo da Silva Gomes <programaker@gmail.com>
2009-12-02 20:21:47 -0200

    rebol oddwords seems to work now

--- rebol/oddwords.r
+++ rebol/oddwords.r
@@ -6,13 +6,12 @@ oddwords: func [phrase] [
     
     for i 2 length? words 2 [
         word: pick words i
-        ;print word
-        ;print find text word
-        reverse/part (find text word) (length? word)
+        reverse/part (find/last text word) (length? word)
     ]
     
     text
 ]
 
-;print oddwords "foo"
+print oddwords "foo"
 print oddwords "this is a simple phrase"
+print oddwords "rebol is so cool"
